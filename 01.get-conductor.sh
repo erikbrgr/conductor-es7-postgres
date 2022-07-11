789d04f8a29596475b4cb72fa333a5dbd3e1a0a6
Amit Mor <amitm@at-bay.com>
2022-07-11 21:54:17 +0300

    removed 3.5.1 patches
    Signed-off-by: Amit Mor <amitm@at-bay.com>

--- 01.get-conductor.sh
+++ 01.get-conductor.sh
@@ -25,9 +25,5 @@ rm -r es6-persistence
 git apply ../${CONDUCTOR_VERSION}-update-to-es7.patch
 #### Set ownerEmailMandatory to false
 git apply ../${CONDUCTOR_VERSION}-ownerEmailmandatory-to-false.patch
-#### Remove .schemas() call to prevent the currentSchema property in the Postgres connection string being overridden
-###git apply ../${CONDUCTOR_VERSION}-fix-currentSchema-override.patch
-#### Update colors so we can distinguish between IN_PROGRESS and SCHEDULED tasks
-###git apply ../${CONDUCTOR_VERSION}-update-colors.patch
 #### "Clean" changes made by patches so, if we make new changes, we can easily create another patch
 git commit -a -m "Applied patches"
