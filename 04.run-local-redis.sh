5bd99e900821888b23c55e7212b1b722fbd2de2d
Amit Mor <amit.mor@hotmail.com>
2022-07-16 12:56:31 +0000

    working redis with compose

--- 04.run-local-redis.sh
+++ 04.run-local-redis.sh
@@ -2,6 +2,6 @@
 
 cd docker/
 
-docker-compose -f docker-compose.yaml -f docker-compose-redis.yaml up -d redis
-docker-compose -f docker-compose.yaml -f docker-compose-redis.yaml up -d elasticsearch
-docker-compose -f docker-compose.yaml -f docker-compose-redis.yaml up conductor-server 
+#docker-compose -f docker-compose.yaml -f docker-compose-redis.yaml up -d redis
+#docker-compose -f docker-compose.yaml -f docker-compose-redis.yaml up -d elasticsearch
+docker-compose -f docker-compose.yaml -f docker-compose-redis.yaml up 
