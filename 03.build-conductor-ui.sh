b328ff50bffbba016f510629f747b8b897940cf0
Hussein Esa <husseinesa@Husseins-MBP.lan>
2022-07-26 11:30:15 +0300

    fix scripts for local run with redis

--- 03.build-conductor-ui.sh
+++ 03.build-conductor-ui.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+CONDUCTOR_VERSION=$1
+
 # Display commands and exit on error
 set -ex 
 
-cd conductor/docker
+cd conductor-$CONDUCTOR_VERSION/docker
 
 docker build -t conductor:ui -f ui/Dockerfile ../
